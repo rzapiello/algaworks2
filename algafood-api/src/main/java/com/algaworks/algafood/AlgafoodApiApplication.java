0a138c74d557c50c508233b3142184fa06c2a90d
Rodrigo de Lima Zapiello <81442182+rzapiello@users.noreply.github.com>
2022-08-21 13:04:03 -0300

    2.14
    2.14

--- algafood-api/src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ algafood-api/src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- algafood-api/src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -4,13 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+
 @Controller
 public class MeuPrimeiroController {
 	
-	
+	private AtivacaoClienteService  ativacaoClienteService;
+		
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+		this.ativacaoClienteService = ativacaoClienteService;
+		
+		System.out.println("Cliente Service");
+	}
+
+
+
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
+		
+		Cliente joao = new Cliente("João","rzapiello@gmail.com","3464949");
+		ativacaoClienteService.ativar(joao);
+		
 		return "oi!";
 	}
 
